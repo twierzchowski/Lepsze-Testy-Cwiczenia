27a534caf4e0e63faa8f175a27266651dac688a8
Tomasz Wierzchowski <twierzchowski@future-processing.com>
2017-10-04 18:16:57 +0200

    move autoTriage to domain level.

--- Application/UseCases/AutoTriageBugUseCase.cs
+++ Application/UseCases/AutoTriageBugUseCase.cs
@@ -21,42 +21,9 @@ namespace Application.UseCases
             var bug = _bugRepository.GetById(command.Id);
             if (bug == null)
                 throw new Exception($"bug with Id ='{command.Id}' not found");
-
-            var severityFromExternalService = _triageBugService.GetSeverity(bug.Title, bug.Description);
-            var priorityFromExternalService = _triageBugService.GetPriority(bug.Title, bug.Description);
-
-            Severity severity = MapSeverityFromExternalService(severityFromExternalService);
-            Priority priority = MapPriorityFromExternalService(priorityFromExternalService);
             
-            bug.Triage(severity, priority);
+            bug.AutoTriage(_triageBugService);
             _unitOfWork.Save();
-        }
-
-        private Priority MapPriorityFromExternalService(int priorityFromExternalService)
-        {
-            switch (priorityFromExternalService)
-            {
-                case 1:
-                    return Priority.High;
-                case 2:
-                    return Priority.Medium;
-                case 3:
-                    return Priority.Low;
-                default:
-                    throw new Exception("Invalid priority value");
-            }
-        }
-
-        private Severity MapSeverityFromExternalService(int severityFromExternalService)
-        {
-            if (severityFromExternalService < 0)
-                throw new Exception("Invalid severity value");
-            if (severityFromExternalService < 100)
-                return Severity.High;
-            if (severityFromExternalService <= 250)
-                return Severity.Medium;
-
-            return Severity.Low;
         }
     }
 }
---
+++ Domain/TriageBugService.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Net.Http;
+
+namespace Domain
+{
+        public class TriageBugService : ITriageBugService
+        {
+            private readonly string _baseUrl = ConfigurationManager.AppSettings["AutoTriageServiceUrl"];
+
+            public int GetSeverity(string title, string description)
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = $"{_baseUrl}/api/severity?title={title}&description={description}";
+                    return int.Parse(client.GetStringAsync(url).Result);
+                }
+            }
+
+            public int GetPriority(string title, string description)
+            {
+                using (var client = new HttpClient())
+                {
+                    string url = $"{_baseUrl}/api/priority?title={title}&description={description}";
+                    return int.Parse(client.GetStringAsync(url).Result);
+                }
+            }
+        }
+}
